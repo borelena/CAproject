fcb53da7c03db57a195088ac6ebb571e1bc6b0ef
Elena Borer <elena.borer@gmail.com>
2018-10-09 12:28:03 +0300

    crypto functions demo
    it was created a simple contract to get crypto hases

--- cryptoDemo.sol
+++ cryptoDemo.sol
@@ -1,7 +1,9 @@
 pragma solidity ^0.4.24;
 
+/*contract is created to test crypto functions*/
 contract CryptoFunctions {
-    
+  
+  
   function cryptoSHA256(string _value) external pure returns (bytes32) {
 	 return	sha256(abi.encode(_value));
 }
